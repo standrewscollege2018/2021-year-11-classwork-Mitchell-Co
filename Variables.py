d55288f65ac087d3186f3d1df6142dde734aeb42
Mitchell-Co <78179598+Mitchell-Co@users.noreply.github.com>
2021-02-01 09:38:07 +1300

    Name age

---
+++ Variables.py
@@ -0,0 +1,10 @@
+# This program demonstrates storing information in variables
+# Here a variable called name is storing the text "Tyrone"
+
+name="Tyrone"
+
+print("Hello", name)
+
+# here's a variable storing an integer
+number1 = 13
+print(number1 %2)
---
+++ input.py
@@ -0,0 +1,14 @@
+# This program enables the user to enter their namedtuple
+# The program then greets them by namedtuple
+
+name = input("What is your name?")
+
+print("hello", name, ",How are you",)
+
+age = input ("how old are you")
+
+print("Your name is", name, "and you are" , age, "year old")
+
+
+
+
